efd08f0e72961be9536b14ae0c3adff4c6b01482
Manuel Osvaldo Olguín <molguin@kth.se>
2018-04-03 01:32:58 +0200

    on/off!

--- const.py
+++ const.py
@@ -1,2 +1,18 @@
 KASA_URL = 'https://wap.tplinkcloud.com'
 KASA_DVC_LIST_REQ = {'method': 'getDeviceList'}
+KASA_APP_SERVER_URL = 'https://eu-wap.tplinkcloud.com'
+KASA_DVC_CONTROL_REQ = {
+    'method': 'passthrough',
+    'params': {
+        'deviceId': None,
+        'requestData': None
+    }
+}
+
+KASA_REQ_DATA_DVC_CONTROL = {
+    'system': {
+        'set_relay_state': {
+            'state': None
+        }
+    }
+}
--- controlKASA.py
+++ controlKASA.py
@@ -10,11 +10,11 @@ def control():
 
 @control.command('DeviceList')
 def getDeviceList() -> None:
-    token = getKasaAPIToken()
-    params = {'token': token}
+    params = {'token': getKasaAPIToken()}
 
     resp = req.post(KASA_URL, params=params, json=KASA_DVC_LIST_REQ)
     devices = resp.json()['result']['deviceList']
+    print(resp.json())
 
     print('{:40s} \t {:40s} \t {:40s}'.format('Device', 'Model', 'ID'))
     for dev in devices:
@@ -22,6 +22,32 @@ def getDeviceList() -> None:
                                                   (dev['deviceModel'] + ' ' +
                                                    dev['deviceName']),
                                                   dev['deviceId']))
+
+
+@control.command('TurnOff')
+@click.argument('device')
+def turnOff(device: str) -> None:
+    KASA_REQ_DATA_DVC_CONTROL['system']['set_relay_state']['state'] = 0
+    KASA_DVC_CONTROL_REQ['params']['deviceId'] = device
+    KASA_DVC_CONTROL_REQ['params'] \
+        ['requestData'] = json.dumps(KASA_REQ_DATA_DVC_CONTROL)
+
+    params = {'token': getKasaAPIToken()}
+    resp = req.post(KASA_APP_SERVER_URL,
+                    params=params, json=KASA_DVC_CONTROL_REQ)
+
+
+@control.command('TurnOn')
+@click.argument('device')
+def turnOn(device: str) -> None:
+    KASA_REQ_DATA_DVC_CONTROL['system']['set_relay_state']['state'] = 1
+    KASA_DVC_CONTROL_REQ['params']['deviceId'] = device
+    KASA_DVC_CONTROL_REQ['params'] \
+        ['requestData'] = json.dumps(KASA_REQ_DATA_DVC_CONTROL)
+
+    params = {'token': getKasaAPIToken()}
+    resp = req.post(KASA_APP_SERVER_URL,
+                    params=params, json=KASA_DVC_CONTROL_REQ)
 
 
 if __name__ == '__main__':
